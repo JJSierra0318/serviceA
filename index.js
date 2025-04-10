5b4fc6dc4ee41a215030250ec9ce7cf7feeecbca
Juan José Sierra <juanjosesierram@gmail.com>
2025-04-10 15:39:32 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import serviceB from 'serviceB';
+
+const app = express();
+
+// routes for the other service are:
+// .../services/serviceB
+// .../services/serviceC
+//
+app.use('/services', serviceB);
+
+app.get('/', (req, res) => {
+  res.send('service A root');
+});
+
+app.listen(4000, () => {
+  console.log('Server running at http://localhost:4000');
+});
